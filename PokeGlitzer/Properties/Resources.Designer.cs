75f1f58c81468f12bb3b0bc5db15e9c59897fe9c
Mickael Laurent <esh4rk@gmail.com>
2022-07-18 20:09:11 +0200

    add text resources

--- PokeGlitzer/Properties/Resources.Designer.cs
+++ PokeGlitzer/Properties/Resources.Designer.cs
@@ -61,6 +61,198 @@ namespace PokeGlitzer.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to (None)
+        ///Master Ball
+        ///Ultra Ball
+        ///Great Ball
+        ///Poké Ball
+        ///Safari Ball
+        ///Net Ball
+        ///Dive Ball
+        ///Nest Ball
+        ///Repeat Ball
+        ///Timer Ball
+        ///Luxury Ball
+        ///Premier Ball
+        ///Potion
+        ///Antidote
+        ///Burn Heal
+        ///Ice Heal
+        ///Awakening
+        ///Parlyz Heal
+        ///Full Restore
+        ///Max Potion
+        ///Hyper Potion
+        ///Super Potion
+        ///Full Heal
+        ///Revive
+        ///Max Revive
+        ///Fresh Water
+        ///Soda Pop
+        ///Lemonade
+        ///Moomoo Milk
+        ///EnergyPowder
+        ///Energy Root
+        ///Heal Powder
+        ///Revival Herb
+        ///Ether
+        ///Max Ether
+        ///Elixir
+        ///Max Elixir
+        ///Lava Cookie
+        ///Blue Flute
+        ///Yellow Flute
+        ///Red Flute
+        ///Black Flute
+        ///White Flut [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string items {
+            get {
+                return ResourceManager.GetString("items", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Littleroot Town
+        ///Oldale Town
+        ///Dewford Town
+        ///Lavaridge Town
+        ///Fallarbor Town
+        ///Verdanturf Town
+        ///Pacifidlog Town
+        ///Petalburg City
+        ///Slateport City
+        ///Mauville City
+        ///Rustboro City
+        ///Fortree City
+        ///Lilycove City
+        ///Mossdeep City
+        ///Sootopolis City
+        ///Ever Grande City
+        ///Route 101
+        ///Route 102
+        ///Route 103
+        ///Route 104
+        ///Route 105
+        ///Route 106
+        ///Route 107
+        ///Route 108
+        ///Route 109
+        ///Route 110
+        ///Route 111
+        ///Route 112
+        ///Route 113
+        ///Route 114
+        ///Route 115
+        ///Route 116
+        ///Route 117
+        ///Route 118
+        ///Route 119
+        ///Route 120
+        ///Route 121
+        ///Route 122
+        ///Route 123
+        ///Route 12 [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string locations {
+            get {
+                return ResourceManager.GetString("locations", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ———
+        ///Pound
+        ///Karate Chop
+        ///Double Slap
+        ///Comet Punch
+        ///Mega Punch
+        ///Pay Day
+        ///Fire Punch
+        ///Ice Punch
+        ///Thunder Punch
+        ///Scratch
+        ///Vise Grip
+        ///Guillotine
+        ///Razor Wind
+        ///Swords Dance
+        ///Cut
+        ///Gust
+        ///Wing Attack
+        ///Whirlwind
+        ///Fly
+        ///Bind
+        ///Slam
+        ///Vine Whip
+        ///Stomp
+        ///Double Kick
+        ///Mega Kick
+        ///Jump Kick
+        ///Rolling Kick
+        ///Sand Attack
+        ///Headbutt
+        ///Horn Attack
+        ///Fury Attack
+        ///Horn Drill
+        ///Tackle
+        ///Body Slam
+        ///Wrap
+        ///Take Down
+        ///Thrash
+        ///Double-Edge
+        ///Tail Whip
+        ///Poison Sting
+        ///Twineedle
+        ///Pin Missile
+        ///Leer
+        ///Bite
+        ///Growl
+        ///Roar
+        ///Sing
+        ///Supersonic
+        ///Sonic Boom
+        ///Dis [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string moves {
+            get {
+                return ResourceManager.GetString("moves", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hardy
+        ///Lonely
+        ///Brave
+        ///Adamant
+        ///Naughty
+        ///Bold
+        ///Docile
+        ///Relaxed
+        ///Impish
+        ///Lax
+        ///Timid
+        ///Hasty
+        ///Serious
+        ///Jolly
+        ///Naive
+        ///Modest
+        ///Mild
+        ///Quiet
+        ///Bashful
+        ///Rash
+        ///Calm
+        ///Gentle
+        ///Sassy
+        ///Careful
+        ///Quirky.
+        /// </summary>
+        internal static string natures {
+            get {
+                return ResourceManager.GetString("natures", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Egg
         ///Bulbasaur
         ///Ivysaur

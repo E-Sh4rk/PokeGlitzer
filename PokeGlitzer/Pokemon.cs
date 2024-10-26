bfd211b0a319dcf02f13280d93ba6777d440edeb
Mickael LAURENT <esh4rk@gmail.com>
2024-10-26 14:08:32 +0200

    add tooltip to otid field

--- PokeGlitzer/Pokemon.cs
+++ PokeGlitzer/Pokemon.cs
@@ -1,7 +1,4 @@
-﻿using Avalonia.Controls;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using SkiaSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
@@ -9,7 +6,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
